6f3d517accbfc20e13fc028b21456c01ddeec041
Blake Arnsdorff <blake@dontpanicux.com>
2023-11-16 14:18:09 -0800

    Initial global vars
    Created variables for each major portion of the UI that I'll need to make interactive or gather data from to display.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 import { tweetsData } from './data.js'
 
+const tweetInputArea = document.getElementById('tweet-input-area')
+const tweetInput = document.getElementById('tweet-input')
+const tweetBtn = document.getElementById('tweet-btn')
+const feed = document.getElementById('feed')
+
+tweetBtn.addEventListener('click', function(){
+    console.log(`clicked ${tweetBtn}`)
+})
