d6a782f556595fd8f80c33e9d0fc5787ee76d5b8
Blake Arnsdorff <blake@dontpanicux.com>
2023-11-16 15:19:01 -0800

    forEach for rendering tweetsData in UI

--- index.js
+++ index.js
@@ -8,3 +8,33 @@ const feed = document.getElementById('feed')
 tweetBtn.addEventListener('click', function(){
     console.log(`clicked ${tweetBtn}`)
 })
+
+tweetsData.forEach(getFeedHtml)
+/*
+    for each Object in tweetsData array
+    use object values to populate UI
+*/
+function getFeedHtml(tweet){
+    feed.innerHTML += `
+    <div class="tweet">
+        <div class="tweet-inner">
+            <img src="./${tweet.profilePic}" class="profile-pic">
+            <div>
+                <p class="handle">${tweet.handle}</p>
+                <p class="tweet-text">${tweet.tweetText}</p>
+                <div class="tweet-details">
+                    <span class="tweet-detail">
+                        ${tweet.replies.length}
+                    </span>
+                    <span class="tweet-detail">
+                        ${tweet.likes}
+                    </span>
+                    <span class="tweet-detail">
+                        ${tweet.retweets}
+                    </span>
+                </div>   
+            </div>            
+        </div>
+    </div>
+`
+}
