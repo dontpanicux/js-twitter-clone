03d7902a5a9a8abd5df3df4a4020342668ba94d1
Blake Arnsdorff <blake@dontpanicux.com>
2023-11-17 09:42:04 -0800

    Refactored getFeedHtml()
    I'm struggling with the appropriate 'scoping' of variables within nested functions.
    
    This function works & I can render the data structure to the DOM appropriately, but I'm unable, for now, to understand how I can write cleaner code.
    
    I'd like for my tweetsData.forEach() to call a 'named' function versus having an anonymous function, but this is where my understanding of appropriately scoping breakdown, and I'm unable to return the appropriate variables 'where' I need them to be accessible.

--- index.js
+++ index.js
@@ -4,42 +4,52 @@ const tweetInputArea = document.getElementById('tweet-input-area')
 const tweetInput = document.getElementById('tweet-input')
 const tweetBtn = document.getElementById('tweet-btn')
 const feed = document.getElementById('feed')
+// let feedHtml =''
 
 tweetBtn.addEventListener('click', function(){
     console.log(`clicked ${tweetBtn}`)
 })
 
-tweetsData.forEach(getFeedHtml)
+function getFeedHtml(){
+
+    let feedHtml =''
+    
+    tweetsData.forEach(function(tweet){ 
+        feedHtml += `
+            <div class="tweet">
+                <div class="tweet-inner">
+                    <img src="./${tweet.profilePic}" class="profile-pic">
+                    <div>
+                        <p class="handle">${tweet.handle}</p>
+                        <p class="tweet-text">${tweet.tweetText}</p>
+                        <div class="tweet-details">
+                            <span class="tweet-detail">
+                                ${tweet.replies.length}
+                            </span>
+                            <span class="tweet-detail">
+                                ${tweet.likes}
+                            </span>
+                            <span class="tweet-detail">
+                                ${tweet.retweets}
+                            </span>
+                        </div>   
+                    </div>            
+                </div>
+            </div>
+            `
+        //console.log(feedHtml)
+    
+    })
+    return feedHtml
+
+}
+    
+
 /*
     for each Object in tweetsData array
     use object values to populate UI
 */
 
-function getFeedHtml(tweet){
-    let feedHtml = ''
-    
-    feedHtml += `
-    <div class="tweet">
-        <div class="tweet-inner">
-            <img src="./${tweet.profilePic}" class="profile-pic">
-            <div>
-                <p class="handle">${tweet.handle}</p>
-                <p class="tweet-text">${tweet.tweetText}</p>
-                <div class="tweet-details">
-                    <span class="tweet-detail">
-                        ${tweet.replies.length}
-                    </span>
-                    <span class="tweet-detail">
-                        ${tweet.likes}
-                    </span>
-                    <span class="tweet-detail">
-                        ${tweet.retweets}
-                    </span>
-                </div>   
-            </div>            
-        </div>
-    </div>
-`
-//console.log(feedHtml)
-return feedHtml
-}
+
+// add existing content to #feed
+feed.innerHTML = getFeedHtml()
